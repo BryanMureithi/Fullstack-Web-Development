4ec8a4c88d82d4592932e739cd82cf070a189cab
BryanMureithi <brymureithi@gmail.com>
2023-08-23 19:06:30 +0300

    step2 done

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,10 +6,15 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault();
+
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    }
     const personObject = {
       name: newName,
     };
     setPersons(persons.concat(personObject));
+    console.log(persons);
     setNewName("");
   };
 
